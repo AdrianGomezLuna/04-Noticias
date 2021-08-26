106e75d5f36595147fd1ee0497412d33e5f9c09f
Adrian Gomez Luna <agomezluna@iessansebastian.com>
2021-08-26 11:38:29 +0200

    Interfaces para el manejo de la informacion

--- src/app/services/noticias.service.ts
+++ src/app/services/noticias.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { RespuestaTopHeadlines } from '../pages/interfaces/interfaces';
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +10,6 @@ export class NoticiasService {
   constructor(private http: HttpClient) { }
 
   getTopHeadlines() {
-    return this.http.get(`https://newsapi.org/v2/top-headlines?country=us&apiKey=c8dc81aa05c145b89d63123af110a184`);
+    return this.http.get<RespuestaTopHeadlines>(`https://newsapi.org/v2/top-headlines?country=us&apiKey=c8dc81aa05c145b89d63123af110a184`);
   }
 }
