4471951e49086eda12c0b3e3fd104af7d0246c67
Adrian Gomez Luna <agomezluna@iessansebastian.com>
2021-08-27 16:26:43 +0200

    Plugin para navegador noticias en el Buscador

--- src/app/components/noticia/noticia.component.ts
+++ src/app/components/noticia/noticia.component.ts
@@ -1,6 +1,9 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Article } from '../../pages/interfaces/interfaces';
 
+import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
+
+
 @Component({
   selector: 'app-noticia',
   templateUrl: './noticia.component.html',
@@ -11,8 +14,14 @@ export class NoticiaComponent implements OnInit {
   @Input() noticia: Article;
   @Input() indice: number;
 
-  constructor() { }
+  constructor( private iab: InAppBrowser) { }
 
   ngOnInit() {}
+
+  abrirNoticia(){
+    // console.log('Noticia:', this.noticia.url);
+    const browser = this.iab.create(this.noticia.url, '_system');
+  }
+
 
 }
